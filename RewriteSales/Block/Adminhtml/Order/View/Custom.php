c9c1dbb1233ea1335d835f90622fe1bbbe433c1a
Md. Sohel Rana <sohelrana09@gmail.com>
2015-10-02 22:19:47 +0600

    How to overwrite sales view order phtml file in magento2

---
+++ RewriteSales/Block/Adminhtml/Order/View/Custom.php
@@ -0,0 +1,7 @@
+<?php
+namespace SR\RewriteSales\Block\Adminhtml\Order\View;
+
+class Custom extends \Magento\Backend\Block\Template
+{
+
+}
---
+++ RewriteSales/Block/Adminhtml/Order/View/Info.php
@@ -0,0 +1,7 @@
+<?php
+namespace SR\RewriteSales\Block\Adminhtml\Order\View;
+
+class Info extends \Magento\Sales\Block\Adminhtml\Order\View\Info
+{
+
+}
